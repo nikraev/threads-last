3ed8e436f9d5cdbe589fcabee08af37242c454dd
nikraev <nikraev@yandex.ru>
2015-07-07 16:19:39 +0300

    add rabbit lib

--- lib/rabbitmq.rb
+++ lib/rabbitmq.rb
@@ -1,16 +1,43 @@
 require 'bunny'
 
 class RabbitQueue
-  def initialize(host)
+  def initialize(host, queue)
     @host = host
+    @queue = queue
   end
   
-  def getInformation()
+  def getData()
+      
+      self.getChannel() unless @channel
+      
+      puts @channel
+      
+      queue = @channel.queue("#{@queue}")
+      queue.subscribe(:block => true) do |delivery_info, properties, body|
+      return body     
+      end  
     
   end
   
 protected 
-  def connectRabbitMQ()
+
+  def getChannel
+    #if(!connect = self.connectRabbitMQ())    
+       connect=self.connectRabbitMQ()
+       @channel = connect.create_channel
+       
+    #end   
     
+   
+  end
+  
+  def connectRabbitMQ()
+    connect = Bunny.new
+    connect.start
+    return connect 
   end
 end
+
+r = RabbitQueue.new('localhost','hello')
+body = r.getData()
+puts body
